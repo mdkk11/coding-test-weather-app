d9ae2ba94ad6e80a770ff035732409ca66f50233
mk <mk.eng1119@gmail.com>
2024-09-22 13:41:17 +0900

    chore: 絶対パスでimportできるよう設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 import react from "@vitejs/plugin-react-swc"
+import path from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "happy-dom",
