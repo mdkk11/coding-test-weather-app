a5bb361c20cf6becedc599bf7b235062cb9d03cb
mk <mk.eng1119@gmail.com>
2024-09-23 18:38:41 +0900

    feat: Headingコンポーネント作成

---
+++ src/components/ui/Heading/index.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Heading } from "."
+
+const meta = {
+  title: "UI/Heading",
+  component: Heading,
+  tags: ["autodocs"],
+  args: { level: "h1", children: "見出し".repeat(3) },
+} satisfies Meta<typeof Heading>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const Bold: Story = {
+  args: { bold: true },
+}
+
+export const Small: Story = {
+  args: { size: "sm" },
+}
+
+export const Medium: Story = {
+  args: { size: "md" },
+}
+
+export const Large: Story = {
+  args: { size: "lg" },
+}
+
+export const XLarge: Story = {
+  args: { size: "xl" },
+}
+
+export const XXLarge: Story = {
+  args: { size: "2xl" },
+}
+
+export const XXXLarge: Story = {
+  args: { size: "3xl" },
+}
