c8fafa48ca644193fddfc1b88ec1900da2022ed8
mk <mk.eng1119@gmail.com>
2024-09-23 20:09:42 +0900

    feat: ページに表示する

--- src/app/pages/weather_forecast.tsx
+++ src/app/pages/weather_forecast.tsx
@@ -1,5 +1,9 @@
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+
 import { Section } from "@/components/ui/Section"
 import * as LocationInput from "@/features/weather/components/location_input"
+import * as WeatherOverview from "@/features/weather/components/weather_overview"
 import { LocationContextProvider } from "@/features/weather/providers/location/provider"
 
 export default function WeatherForecastPage() {
@@ -7,6 +11,11 @@ export default function WeatherForecastPage() {
     <LocationContextProvider>
       <Section>
         <LocationInput.Container />
+        <ErrorBoundary fallbackRender={WeatherOverview.Error}>
+          <Suspense fallback={<WeatherOverview.Loading />}>
+            <WeatherOverview.Container />
+          </Suspense>
+        </ErrorBoundary>
       </Section>
     </LocationContextProvider>
   )
